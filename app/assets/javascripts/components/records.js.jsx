83e7af3b06d04f20ea93221ad4977ec997e50bf2
Moses Kim <moseskim92@yahoo.com>
2016-10-17 14:23:03 -0700

    changed the db from sqlite to postgres

---
+++ app/assets/javascripts/components/records.js.jsx
@@ -0,0 +1,41 @@
+var Records = React.createClass({
+  getInitialState() {
+    return { records: this.props.data };
+  },
+
+  getDefaultProps() {
+    return { records: []};
+  },
+
+  addRecord(record) {
+    var records = this.state.records.slice();
+    records.push(record);
+    this.setState({ records: records});
+  },
+
+  render: function() {
+    return (
+      <div className='records'>
+        <h2 className='title'>
+          Expense Report
+        </h2>
+        <RecordForm handleNewRecord={this.addRecord} />
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Title</th>
+              <th>Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.records.map(function(record) {
+              return <Record key={record.id} record={record} />
+             })}
+          </tbody>
+        </table>
+      </div>
+
+    );
+  }
+});
